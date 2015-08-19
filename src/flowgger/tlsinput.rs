56e053ade9dd9daace21edc09c136f5bfa4a462e
Frank Denis <github@pureftpd.org>
2015-08-20 01:58:20 +0200

    Add a new input.framed preference to accept framed messages over TLS
    That might be eventually moved to the rfc5424 parser, because less
    horrible formats don't need a horrible way to send a message length.
    
    The message length is parsed but ignored. No newlines in rfc5424 log
    messages for now.

--- src/flowgger/tlsinput.rs
+++ src/flowgger/tlsinput.rs
@@ -7,11 +7,13 @@ use openssl::x509::X509FileType;
 use std::io::{stderr, Write, BufRead, BufReader};
 use std::net::{TcpListener, TcpStream};
 use std::path::Path;
+use std::str;
 use std::sync::mpsc::SyncSender;
 use std::thread;
 
 const DEFAULT_CERT: &'static str = "flowgger.pem";
 const DEFAULT_CIPHERS: &'static str = "ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SH";
+const DEFAULT_FRAMED: bool = true;
 const DEFAULT_KEY: &'static str = "flowgger.pem";
 const DEFAULT_LISTEN: &'static str = "0.0.0.0:6514";
 
@@ -19,7 +21,8 @@ const DEFAULT_LISTEN: &'static str = "0.0.0.0:6514";
 struct TlsConfig {
     cert: String,
     key: String,
-    ciphers: String
+    ciphers: String,
+    framed: bool
 }
 
 pub struct TlsInput {
@@ -33,11 +36,13 @@ impl Input for TlsInput {
         let cert = config.lookup("input.tls_cert").map_or(DEFAULT_CERT, |x| x.as_str().unwrap()).to_owned();
         let key = config.lookup("input.tls_key").map_or(DEFAULT_KEY, |x| x.as_str().unwrap()).to_owned();
         let ciphers = config.lookup("input.tls_ciphers").map_or(DEFAULT_CIPHERS, |x| x.as_str().unwrap()).to_owned();
+        let framed = config.lookup("input.framed").map_or(DEFAULT_FRAMED, |x| x.as_bool().unwrap());
 
         let tls_config = TlsConfig {
             cert: cert,
             key: key,
-            ciphers: ciphers
+            ciphers: ciphers,
+            framed: framed
         };
         TlsInput {
             listen: listen,
@@ -63,6 +68,23 @@ impl Input for TlsInput {
     }
 }
 
+fn read_msglen(reader: &mut BufRead) -> Result<usize, &'static str> {
+    let mut nbytes_v = Vec::with_capacity(16);
+    let nbytes_vl = match reader.read_until(b' ', &mut nbytes_v) {
+        Err(_) | Ok(0) | Ok(1) => return Err("EOF"),
+        Ok(nbytes_vl) => nbytes_vl
+    };
+    let nbytes_s = match str::from_utf8(&nbytes_v[..nbytes_vl - 1]) {
+        Err(_) => return Err("Invalid message length"),
+        Ok(nbytes_s) => nbytes_s
+    };
+    let nbytes: usize = match nbytes_s.parse() {
+        Err(_) => return Err("Invalid message length"),
+        Ok(nbytes) => nbytes
+    };
+    Ok(nbytes)
+}
+
 fn handle_client<TE>(client: TcpStream, tx: SyncSender<Vec<u8>>, decoder: Box<Decoder>, encoder: TE, tls_config: TlsConfig) where TE: Encoder {
     let mut ctx = SslContext::new(Tlsv1_2).unwrap();
     ctx.set_verify(SSL_VERIFY_PEER, None); // Sigh
@@ -71,14 +93,31 @@ fn handle_client<TE>(client: TcpStream, tx: SyncSender<Vec<u8>>, decoder: Box<De
     ctx.set_private_key_file(&Path::new(&tls_config.key), X509FileType::PEM).unwrap();
     ctx.set_cipher_list(&tls_config.ciphers).unwrap();
     let sslclient = SslStream::accept(&ctx, client).unwrap();
-    let reader = BufReader::new(sslclient);
-    for line in reader.lines() {
-        let line = match line {
-            Err(_) => return,
-            Ok(line) => line
-        };
-        if let Err(e) = handle_line(&line, &tx, &decoder, &encoder) {
-            let _ = writeln!(stderr(), "{}: [{}]", e, line.trim());
+    let mut reader = BufReader::new(sslclient);
+    if tls_config.framed == false {
+        for line in reader.lines() {
+            let line = match line {
+                Err(_) => return,
+                Ok(line) => line
+            };
+            if let Err(e) = handle_line(&line, &tx, &decoder, &encoder) {
+                let _ = writeln!(stderr(), "{}: [{}]", e, line.trim());
+            }
+        }
+    } else {
+        loop {
+            if let Err(e) = read_msglen(&mut reader) {
+                let _ = writeln!(stderr(), "{}", e);
+                return;
+            }
+            let mut line = String::new();
+            if reader.read_line(&mut line).is_err() {
+                println!("err");
+                return;
+            }
+            if let Err(e) = handle_line(&line, &tx, &decoder, &encoder) {
+                let _ = writeln!(stderr(), "{}: [{}]", e, line.trim());
+            }
         }
     }
 }
