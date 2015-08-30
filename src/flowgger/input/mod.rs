715af1ee65db18047792cea5d4e91d9f805aeba2
Frank Denis <github@pureftpd.org>
2015-08-30 20:08:03 +0200

    The Input trait doesn't need to be clonable/sendable

--- src/flowgger/input/mod.rs
+++ src/flowgger/input/mod.rs
@@ -5,22 +5,6 @@ use flowgger::decoder::Decoder;
 use flowgger::encoder::Encoder;
 use std::sync::mpsc::SyncSender;
 
-pub trait CloneBoxedInput {
-    fn clone_boxed<'a>(&self) -> Box<Input + Send + 'a> where Self: 'a;
-}
-
-impl<T: Input + Clone + Send> CloneBoxedInput for T {
-    fn clone_boxed<'a>(&self) -> Box<Input + Send + 'a> where Self: 'a {
-        Box::new(self.clone())
-    }
-}
-
-impl Clone for Box<Input> {
-    fn clone(&self) -> Box<Input> {
-        self.clone_boxed()
-    }
-}
-
-pub trait Input : CloneBoxedInput {
+pub trait Input {
     fn accept(&self, tx: SyncSender<Vec<u8>>, decoder: Box<Decoder + Send>, encoder: Box<Encoder + Send>);
 }
