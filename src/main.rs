5b3558b9ff72bbfbc3a0cb01b5f27b76c60318df
Frank Denis <github@pureftpd.org>
2015-09-02 18:07:43 +0200

    Make the version string a variable, at least.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,10 @@ extern crate toml;
 mod flowgger;
 
 const DEFAULT_CONFIG_FILE: &'static str = "flowgger.toml";
+const FLOWGGER_VERSION_STRING: &'static str = "0.1.2";
 
 fn main() {
-    println!("Starting flowgger 0.1.2");
+    println!("Flowgger v{}", FLOWGGER_VERSION_STRING);
     let config_file = std::env::args().skip(1).next().unwrap_or(DEFAULT_CONFIG_FILE.to_owned());
     flowgger::start(&config_file);
 }
