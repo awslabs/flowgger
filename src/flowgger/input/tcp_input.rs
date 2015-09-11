a10d46b3891eaa85fa8793a8e6227ac618a54af3
Frank Denis <github@pureftpd.org>
2015-09-11 15:54:21 +0200

    Handle configurable framing types in plain TCP, too. Quite a bit redundancy here, but this is okay. Having to specify how frames are delimited is unlikely to be needed with other input types.

--- src/flowgger/input/tcp_input.rs
+++ src/flowgger/input/tcp_input.rs
@@ -3,6 +3,8 @@ use flowgger::decoder::Decoder;
 use flowgger::encoder::Encoder;
 use flowgger::splitter::Splitter;
 use flowgger::splitter::line_splitter::LineSplitter;
+use flowgger::splitter::nul_splitter::NulSplitter;
+use flowgger::splitter::syslen_splitter::SyslenSplitter;
 use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
 use std::sync::mpsc::SyncSender;
@@ -10,17 +12,36 @@ use std::thread;
 use super::Input;
 
 const DEFAULT_LISTEN: &'static str = "0.0.0.0:6514";
+const DEFAULT_FRAMING: &'static str = "line";
+
+#[derive(Clone)]
+pub struct TcpConfig {
+    framing: String
+}
 
 pub struct TcpInput {
-    listen: String
+    listen: String,
+    tcp_config: TcpConfig
 }
 
 impl TcpInput {
     pub fn new(config: &Config) -> TcpInput {
         let listen = config.lookup("input.listen").map_or(DEFAULT_LISTEN, |x|x.as_str().
             expect("input.listen must be an ip:port string")).to_owned();
+        let framing = if config.lookup("input.framed").map_or(false, |x| x.as_bool().
+            expect("input.framed must be a boolean")) {
+            "syslen"
+        } else {
+            DEFAULT_FRAMING
+        };
+        let framing = config.lookup("input.framing").map_or(framing, |x| x.as_str().
+            expect(r#"input.framing must be a string set to "line", "nul" or "syslen""#)).to_owned();
+        let tcp_config = TcpConfig {
+            framing: framing
+        };
         TcpInput {
-            listen: listen
+            listen: listen,
+            tcp_config: tcp_config
         }
     }
 }
@@ -32,9 +53,10 @@ impl Input for TcpInput {
             match client {
                 Ok(client) => {
                     let tx = tx.clone();
+                    let tcp_config = self.tcp_config.clone();
                     let (decoder, encoder) = (decoder.clone_boxed(), encoder.clone_boxed());
                     thread::spawn(move|| {
-                        handle_client(client, tx, decoder, encoder);
+                        handle_client(client, tx, decoder, encoder, tcp_config);
                     });
                 }
                 Err(_) => { }
@@ -43,11 +65,16 @@ impl Input for TcpInput {
     }
 }
 
-fn handle_client(client: TcpStream, tx: SyncSender<Vec<u8>>, decoder: Box<Decoder>, encoder: Box<Encoder>) {
+fn handle_client(client: TcpStream, tx: SyncSender<Vec<u8>>, decoder: Box<Decoder>, encoder: Box<Encoder>, tcp_config: TcpConfig) {
     if let Ok(peer_addr) = client.peer_addr() {
         println!("Connection over TCP from [{}]", peer_addr);
     }
     let reader = BufReader::new(client);
-    let splitter = Box::new(LineSplitter::new(tx, decoder, encoder)) as Box<Splitter<_>>;
+    let splitter = match &tcp_config.framing as &str {
+        "line" => Box::new(LineSplitter::new(tx, decoder, encoder)) as Box<Splitter<_>>,
+        "syslen" => Box::new(SyslenSplitter::new(tx, decoder, encoder)) as Box<Splitter<_>>,
+        "nul" => Box::new(NulSplitter::new(tx, decoder, encoder)) as Box<Splitter<_>>,
+        _ => panic!("Unsupported framing scheme")
+    };
     splitter.run(reader);
 }
