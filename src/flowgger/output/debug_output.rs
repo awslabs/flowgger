0ec52f8e5cefe215d1481155e4e0614879e7f460
Frank Denis <github@pureftpd.org>
2015-09-28 22:01:37 +0200

    Add a simple debug_lf switch to remove line feeds in the debug output

--- src/flowgger/output/debug_output.rs
+++ src/flowgger/output/debug_output.rs
@@ -1,27 +1,42 @@
 use flowgger::config::Config;
+use std::io::{stdout, Write};
 use std::sync::mpsc::Receiver;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use super::Output;
 
-pub struct DebugOutput;
+const DEBUG_DEFAULT_LF: bool = true;
+
+pub struct DebugOutput {
+    lf: bool
+}
 
 impl DebugOutput {
     pub fn new(config: &Config) -> DebugOutput {
-        let _ = config;
-        DebugOutput
+        let lf = config.lookup("output.debug_lf").map_or(DEBUG_DEFAULT_LF, |x| x.as_bool().
+            expect("output.debug_lf must be a boolean") as bool);
+        DebugOutput {
+            lf: lf
+        }
     }
 }
 
 impl Output for DebugOutput {
     fn start(&self, arx: Arc<Mutex<Receiver<Vec<u8>>>>) {
+        let lf = self.lf;
         thread::spawn(move || {
             loop {
                 let bytes = match { arx.lock().unwrap().recv() } {
                     Ok(line) => line,
                     Err(_) => return
                 };
-                println!("{}", String::from_utf8_lossy(&bytes));
+                let out = String::from_utf8_lossy(&bytes);
+                if lf {
+                    println!("{}", out);
+                } else {
+                    print!("{}", out);
+                    let _ = stdout().flush();
+                }
             }
         });
     }
