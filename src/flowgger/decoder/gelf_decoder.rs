6c4a1880df5e2c8a54a04b141187309c4505c2ff
Frank Denis <github@pureftpd.org>
2016-08-09 23:01:20 +0200

    Straight `\n` characters are *not* acceptable in GELF messages
    Undo the straight `\n` changes from the GELF decoder test.
    
    Keep the rewrite hack around while broken clients get fixed, but remove it
    from the documentation.

--- src/flowgger/decoder/gelf_decoder.rs
+++ src/flowgger/decoder/gelf_decoder.rs
@@ -101,8 +101,7 @@ impl Decoder for GelfDecoder {
 
 #[test]
 fn test_gelf() {
-    let msg = r#"{"version":"1.1", "host": "example.org","short_message": "A short message that helps you identify what is going on", "full_message": "Backtrace here\n
-more stuff", "timestamp": 1385053862.3072, "level": 1, "_user_id": 9001, "_some_info": "foo", "_some_env_var": "bar"}"#;
+    let msg = r#"{"version":"1.1", "host": "example.org","short_message": "A short message that helps you identify what is going on", "full_message": "Backtrace here\n\nmore stuff", "timestamp": 1385053862.3072, "level": 1, "_user_id": 9001, "_some_info": "foo", "_some_env_var": "bar"}"#;
     let res = GelfDecoder.decode(msg).unwrap();
     assert!(res.ts == 1385053862.3072);
     assert!(res.hostname == "example.org");
