6b895bce2149f7d927cd2b5eff54eec814e5837e
Frank Denis <github@pureftpd.org>
2016-11-16 18:46:09 +0100

    Restore the default configuration file
    The ruleset is way to short to justify a YAML definition;
    directly include it in the code.

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,21 @@ extern crate toml;
 
 #[macro_use]
 extern crate clap;
-use clap::App;
+use clap::{Arg, App};
 
 mod flowgger;
 pub use flowgger::record_capnp;
 
+const DEFAULT_CONFIG_FILE: &'static str = "flowgger.toml";
+const FLOWGGER_VERSION_STRING: &'static str = "0.2.3";
+
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
-
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
-
-    let config_file = matches.value_of("INPUT").unwrap();
-
+    let matches = App::new("Flowgger")
+        .version(FLOWGGER_VERSION_STRING)
+        .about("A fast, simple and lightweight data collector")
+        .arg(Arg::with_name("config_file").help("Configuration file").value_name("FILE").index(1))
+        .get_matches();
+    let config_file = matches.value_of("config_file").unwrap_or(DEFAULT_CONFIG_FILE);
+    println!("Flowgger {}", FLOWGGER_VERSION_STRING);
     flowgger::start(config_file)
-
 }
