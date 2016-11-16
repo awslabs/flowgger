7e921dc648011c2ee2476d05d379bd444798acda
salpalvv <gth701m@gmail.com>
2016-11-16 11:32:53 -0500

    adding clap for better cli arg parsing
    removing default config constant
    
    changing default toml back

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,23 @@ extern crate serde;
 extern crate serde_json;
 extern crate toml;
 
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 mod flowgger;
 pub use flowgger::record_capnp;
 
-const DEFAULT_CONFIG_FILE: &'static str = "flowgger.toml";
-const FLOWGGER_VERSION_STRING: &'static str = "0.2.3";
-
 fn main() {
-    println!("Flowgger v{}", FLOWGGER_VERSION_STRING);
-    let config_file = std::env::args().skip(1).next().unwrap_or(DEFAULT_CONFIG_FILE.to_owned());
-    flowgger::start(&config_file);
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // required we could have used an 'if let' to conditionally get the value)
+    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+
+    let config_file = matches.value_of("INPUT").unwrap();
+
+    flowgger::start(config_file)
+
 }
