563f20d88d79314dde1365ae55a70a10726c1e53
Frank Denis <github@pureftpd.org>
2017-01-13 02:23:05 +0100

    distbuilds: compile without default features

--- ci/script.sh
+++ ci/script.sh
@@ -4,18 +4,18 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET --features="without_kafka coio"
-    cross build --target $TARGET --release --features="without_kafka coio"
+    cross build --target $TARGET --no-default-features --features="coio"
+    cross build --target $TARGET --release --no-default-features --features="coio"
 
     if [ -n $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET --features="without_kafka coio"
-    cross test --target $TARGET --release --features="without_kafka coio"
+    cross test --target $TARGET --no-default-features --features="coio"
+    cross test --target $TARGET --release --no-default-features --features="coio"
 
-    cross run --target $TARGET --features="without_kafka coio"
-    cross run --target $TARGET --release --features="without_kafka coio"
+    cross run --target $TARGET --no-default-features --features="coio"
+    cross run --target $TARGET --release --no-default-features --features="coio"
 }
 
 # we don't run the "test phase" when doing deploys
