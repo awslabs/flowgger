cf5e2df68db9753f174277d1aaaaf4dc97ccf96d
Frank Denis <github@pureftpd.org>
2017-01-13 02:34:51 +0100

    Dockerfile: use an explicit features set

--- ci/script.sh
+++ ci/script.sh
@@ -4,18 +4,18 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET --no-default-features --features="coio"
-    cross build --target $TARGET --release --no-default-features --features="coio"
+    cross build --target $TARGET --no-default-features
+    cross build --target $TARGET --release --no-default-features
 
     if [ -n $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET --no-default-features --features="coio"
-    cross test --target $TARGET --release --no-default-features --features="coio"
+    cross test --target $TARGET --no-default-features
+    cross test --target $TARGET --release --no-default-features
 
-    cross run --target $TARGET --no-default-features --features="coio"
-    cross run --target $TARGET --release --no-default-features --features="coio"
+    cross run --target $TARGET --no-default-features
+    cross run --target $TARGET --release --no-default-features
 }
 
 # we don't run the "test phase" when doing deploys
