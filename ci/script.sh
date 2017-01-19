e78f0bbaa905af7a0bf048b655c90a33cd930153
Frank Denis <github@pureftpd.org>
2017-01-19 14:34:25 +0100

    Don't compile production builds in CI for now

--- ci/script.sh
+++ ci/script.sh
@@ -5,17 +5,12 @@ set -ex
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     cross build --target $TARGET --no-default-features
-    cross build --target $TARGET --release --no-default-features
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
     cross test --target $TARGET --no-default-features
-    cross test --target $TARGET --release --no-default-features
-
-    cross run --target $TARGET --no-default-features
-    cross run --target $TARGET --release --no-default-features
 }
 
 # we don't run the "test phase" when doing deploys
