0a77c4e5f6431fbbe892e1a719ae71b4f0452c84
Damian Czaja <trojan295@gmail.com>
2019-10-19 13:10:18 +0200

    add github release

--- ci/script.sh
+++ ci/script.sh
@@ -2,18 +2,12 @@
 
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET --no-default-features
-
-    if [ ! -z $DISABLE_TESTS ]; then
-        return
+    if [ -z $DISABLE_TESTS ]; then
+        cross test --target $TARGET --no-default-features
     fi
 
-    cross test --target $TARGET --no-default-features
+    cross build --target $TARGET --release --no-default-features
 }
 
-# we don't run the "test phase" when doing deploys
-if [ -z $TRAVIS_TAG ]; then
-    main
-fi
+main
