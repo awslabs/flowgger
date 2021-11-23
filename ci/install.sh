a41729f1e5c4f3c2138fc8fb0f44a8c317b4dd4d
Vivien Chene <viviench@amazon.com>
2021-11-23 16:17:27 +0000

    test14

--- ci/install.sh
+++ ci/install.sh
@@ -1,44 +1,33 @@
 set -ex
 
-#main() {
+main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
-    citarget=
+    local target=
     if [ $TRAVIS_OS_NAME = linux ]; then
-        citarget=x86_64-unknown-linux-gnu
-        sudo apt-get update
+        target=x86_64-unknown-linux-gnu
+        # sudo apt-get update
+        #
+        # sudo apt-get install musl-tools -y
+        # sudo apt-get -y install pkg-config openssl libssl-dev
 
-        sudo apt-get install musl-tools -y
-        sudo apt-get -y install pkg-config openssl libssl-dev
-
-        which pkg-config
-        if [ -d "/usr/lib/pkgconfig" ]; then
-            export PKG_CONFIG_LIBDIR=/usr/lib/pkgconfig
-        else
-            export PKG_CONFIG_LIBDIR=/usr/lib/aarch64-linux-gnu/pkgconfig
-        fi
-        ls -la $PKG_CONFIG_LIBDIR
+        # pkfconfig is now installed in usr/lib in some installs
+        # if [ -d "/usr/lib/pkgconfig" ]; then
+        #     export PKG_CONFIG_LIBDIR=/usr/lib/pkgconfig
+        # else
+        #     export PKG_CONFIG_LIBDIR=/usr/lib/aarch64-linux-gnu/pkgconfig
+        # fi
         sort=sort
     else
-        citarget=x86_64-apple-darwin
+        target=x86_64-apple-darwin
         sort=gsort  # for `sort --sort-version`, from brew's coreutils.
     fi
 
-    # This fetches latest stable release
-    cargo install cross
-    # citag=$(git ls-remote --tags --refs --exit-code https://github.com/rust-embedded/cross \
-    #                    | cut -d/ -f3 \
-    #                    | grep -E '^v[0-9.]+$' \
-    #                    | $sort --version-sort \
-    #                    | tail -n1)
-    # echo cross version: $citag
-    # curl -LSfs https://japaric.github.io/trust/install.sh | \
-    #     sh -s -- \
-    #        --force \
-    #        --git japaric/cross \
-    #        --tag $citag \
-    #        --target $citarget
-#}
+    # Cross removed support of openssl, which breaks in musl. We're using an older version.
+    # Eventually we may want to create a specific docker file for cross as mentionned here
+    # https://github.com/rust-embedded/cross/issues/229
+    cargo install --version 0.1.16 cross
+}
 
-#main
+main
